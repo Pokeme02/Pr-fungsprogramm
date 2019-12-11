a9a562949a373e0d46330cf7e80ba0d9d63ef705
DavidBT420 <david.hellmich8@googlemail.com>
2019-12-11 20:08:24 +0100

    Profs und Attacken Objecte
    Alle Profs und deren Attacken, sowie die attacken aller Spicker. Variable LifePiontsProfs deklariert. Angriffsbutton1 Hp label ansprechen

--- backup/pruefungsraum.pas
+++ backup/pruefungsraum.pas
@@ -13,7 +13,7 @@ type
   { TForm2 }
 
   TForm2 = class(TForm)
-    Button1: TButton;
+    AngriffButton1: TButton;
     Button2: TButton;
     Button3: TButton;
     Button4: TButton;
@@ -30,6 +30,8 @@ type
     CheckAttack8: TRadioButton;
     CheckAttack9: TRadioButton;
     Image1: TImage;
+    GegnernameLabel1: TLabel;
+    HPGegnerLabel2: TLabel;
     PageControl1: TPageControl;
     RadioAttack1: TRadioGroup;
     CheckAttack1: TRadioButton;
@@ -39,12 +41,15 @@ type
     RadioAttack2: TRadioGroup;
     RadioAttack3: TRadioGroup;
     RadioAttack4: TRadioGroup;
+    SwitchSpicker1: TRadioButton;
     SpickerTab1: TTabSheet;
     SpickerTab2: TTabSheet;
     SpickerTab3: TTabSheet;
     SpecialSpickerTab: TTabSheet;
+    procedure AngriffButton1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure PageControl1Change(Sender: TObject);
+    procedure SwitchSpicker1Change(Sender: TObject);
   private
 
   public
@@ -59,8 +64,25 @@ type
       OtherAttacks: array[1..4] of array[1..5] of Integer;
       Lifepoints: Integer;
       Typ: String;
-    public
-      constructor Create();
+
+  end;
+    type Profs = class
+    private
+      Name: String;
+      ID: Integer;
+      OwnAttacks: array[1..4] of Integer;
+      Lifepoints: Integer;
+      Typ: String;
+
+  end;
+      type ProfAttacken= class
+    private
+      Name: String;
+      ID: Integer;
+
+      Damage: Integer;
+      Typ: String;
+
   end;
 
   //type Attack = class
@@ -75,12 +97,14 @@ type
 
 var
   Form2: TForm2;
-  IDArray: array[1..6] of integer;
+  IDArray: array[1..3] of integer;
   MatheSpicker: Spicker;
   WISpicker: Spicker;
   GWASpicker: Spicker;
 
 implementation
+
+
 
 {$R *.lfm}
 
@@ -94,10 +118,99 @@ begin
 
 end;
 
-procedure TForm2.FormCreate(Sender: TObject);
+procedure TForm2.SwitchSpicker1Change(Sender: TObject);
 begin
 
+end;
 
+procedure TForm2.FormCreate(Sender: TObject);
+begin
+  if IDArray[1] = 1 then
+    begin
+        SpickerTab1.Caption := 'Mathe';
+    end
+  else if IDArray[1] = 2 then
+    begin
+        SpickerTab1.Caption := 'WI';
+    end
+  else if IDArray[1] = 3 then
+    begin
+        SpickerTab1.Caption := 'GWA';
+    end
+  else if IDArray[1] = 4 then
+    begin
+        SpickerTab1.Caption := 'BWL';
+    end
+  else if IDArray[1] = 5 then
+    begin
+        SpickerTab1.Caption := 'Programmieren';
+    end
+  else if IDArray[1] = 6 then
+    begin
+        SpickerTab1.Caption := 'TI';
+    end;
+
+  if IDArray[2] = 1 then
+    begin
+        SpickerTab2.Caption := 'Mathe';
+    end
+  else if IDArray[2] = 2 then
+    begin
+        SpickerTab2.Caption := 'WI';
+    end
+  else if IDArray[2] = 3 then
+    begin
+        SpickerTab2.Caption := 'GWA';
+    end
+  else if IDArray[2] = 4 then
+    begin
+        SpickerTab2.Caption := 'BWL';
+    end
+  else if IDArray[2] = 5 then
+    begin
+        SpickerTab2.Caption := 'Programmieren';
+    end
+  else if IDArray[2] = 6 then
+    begin
+        SpickerTab2.Caption := 'TI';
+    end;
+
+  if IDArray[3] = 1 then
+    begin
+        SpickerTab3.Caption := 'Mathe';
+    end
+  else if IDArray[3] = 2 then
+    begin
+        SpickerTab3.Caption := 'WI';
+    end
+  else if IDArray[3] = 3 then
+    begin
+        SpickerTab3.Caption := 'GWA';
+    end
+  else if IDArray[3] = 4 then
+    begin
+        SpickerTab3.Caption := 'BWL';
+    end
+  else if IDArray[3] = 5 then
+    begin
+        SpickerTab3.Caption := 'Programmieren';
+    end
+  else if IDArray[3] = 6 then
+    begin
+        SpickerTab3.Caption := 'TI';
+    end;
+end;
+
+procedure TForm2.AngriffButton1Click(Sender: TObject);
+begin
+  if SwitchSpicker1.Checked = True then
+  begin
+    SpickerTab2.Enabled:= True;
+    SpickerTab3.Enabled:= True;
+  end;
+  begin
+    HPGegnerLabel2.caption:='80';
+  end;
 end;
 
 end.
